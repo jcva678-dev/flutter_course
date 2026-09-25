fac5895af909e776afc70126c5397772b28dad30
Juan Camilo Arango Valle <jmilo67@hotmail.com>
2026-09-25 01:38:55 -0500

    :sparkles: Started TokTik App

---
+++ tok_tik_app/lib/main.dart
@@ -0,0 +1,27 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:tok_tik_app/config/theme/app_theme.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TokTik',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme().getTheme(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
